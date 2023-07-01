4bc94d9f1f678201608e98f3f949958ba51afa90
Yeahni921 <96525546+Yeahni921@users.noreply.github.com>
2023-07-01 20:07:26 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,16 +1,11 @@
-def solution(n, lost, reserve): 
-    answer = 0 
+def solution(n, lost, reserve):
+    lost_del = set(lost) - set(reserve)
+    reserve_del = set(reserve) - set(lost)
     
-    reserve_del = set(reserve)-set(lost) 
-    lost_del = set(lost)-set(reserve) 
-    
-    for i in reserve_del: 
-        if i-1 in lost_del: 
-            lost_del.remove(i-1) 
-            
-        elif i+1 in lost_del: 
-            lost_del.remove(i+1) 
-            
+    for i in reserve_del:
+        if i-1 in lost_del:
+            lost_del.remove(i-1)
+        elif i+1 in lost_del:
+            lost_del.remove(i+1)
     answer = n - len(lost_del)
-    
     return answer
