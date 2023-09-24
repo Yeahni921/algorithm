7e815b4f4c68321275e7e827295d4306d38b8d7c
Yeahni921 <96525546+Yeahni921@users.noreply.github.com>
2023-09-24 22:16:25 +0900

    [Bronze III] Title: 최소, 최대, Time: 492 ms, Memory: 148408 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,4 +1,12 @@
 N = int(input())
-num = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
 
-print(min(num), max(num))
+max = numbers[0]
+min = numbers[0]
+
+for i in numbers:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+print(min, max)
