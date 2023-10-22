b501bff221888119f02af28144171e99ae34061d
Yeahni921 <96525546+Yeahni921@users.noreply.github.com>
2023-10-22 20:18:27 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+a = []
+b = []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+    
+for j in range(n):
+    b.append(list(map(int, input().split())))
+    
+for row in range(n):
+    for col in range(m):
+        print(a[row][col] + b[row][col], end = ' ')
